6ff396f511e642839d6cf7221bdfcda30a4a2eae
Breno Brito <breno.silvabrito@hotmail.com>
2022-11-18 13:48:13 +0000

    Change Markers to a functional component

--- components/Markers.js
+++ components/Markers.js
@@ -1,56 +1,53 @@
-import React, {Component} from "react";
-import { Marker, Callout  } from 'react-native-maps';
+import React, { useState } from "react";
+import { Marker, Callout } from 'react-native-maps';
 
-class Markers extends Component{
-    constructor(){
-        super()
-        this.state = {markers: [],}
-    }
+function Markers(props) {
+    const n = props.nav
+    const [markers, setMarkers] = useState([]);
 
-    componentDidMount(){
+    loadMarkersLocations()
+
+    function loadMarkersLocations() {
         fetch('https://gist.githubusercontent.com/saravanabalagi/541a511eb71c366e0bf3eecbee2dab0a/raw/bb1529d2e5b71fd06760cb030d6e15d6d56c34b3/places.json')
             .then(response => response.json())
-            .then(markersLocation => this.setState({ markers: markersLocation }));
+            .then(markersLocation => setMarkers(markersLocation));
     }
 
-    defineColor(id){
-        if(id === 1){
+    function defineColor(id) {
+        if (id === 1) {
             return 'blue'
-        }else if(id === 2){
+        } else if (id === 2) {
             return 'green'
-        }else if(id === 3){
+        } else if (id === 3) {
             return 'yellow'
-        }else if(id === 4){
+        } else if (id === 4) {
             return 'orange'
-        }else if(id === 5){
+        } else if (id === 5) {
             return 'pink'
-        }else if(id === 6){
+        } else if (id === 6) {
             return 'purple'
-        }else if(id === 7){
+        } else if (id === 7) {
             return 'tan'
-        }else if(id === 8){
+        } else if (id === 8) {
             return 'teal'
-        }else{
+        } else {
             return 'red'
         }
     }
 
-    render() {
-        const n = this.props.nav
-        return(
-            <>
-            {this.state.markers.map((marker, index) => (
+    return (
+        <>
+            {markers.map((marker, index) => (
                 <Marker
-                  key={index}
-                  coordinate={{latitude : marker.latitude , longitude : marker.longitude}}
-                  title={marker.name}
-                  pinColor= {this.defineColor(marker.place_type_id)}>
-                <Callout onPress={() => n.navigate('Details')} />
+                    key={index}
+                    coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                    title={marker.name}
+                    pinColor={defineColor(marker.place_type_id)}>
+                    <Callout onPress={() => n.navigate('Details')} />
                 </Marker>
-              ))}
-            </>
-        )
-    }
+            ))}
+        </>
+    )
 }
 
 export default Markers;
