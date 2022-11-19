4ba0c5e3bdf3a8d68aaa80e2b0b4cfc053686bc1
Breno Brito <breno.silvabrito@hotmail.com>
2022-11-19 15:33:12 +0000

    Code Refactoring

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,25 +4,17 @@ import { styles as s } from 'tachyons-react-native'
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import Dropdown from "./Dropdown";
 import ExitDropdown from "./ExitDropdown";
-import SearchBarComp from "./SearchBar";
 
 
 function NavBar({ changeState }) {
   const [dropdownVisible, setDdVisibility] = useState(false);
   const [exitDropdownVisible, setExitDdVisibility] = useState(false);
-  const [searchBarVisible, setSearchBarVisibility] = useState(false);
-
-  // const [searchQuery, setSearchQuery] = useState('');
-  // const [filter, setFilter] = useState(0)
 
   function DropdownVisibility(value) {
     setDdVisibility(value)
   }
   function ExitDdVisibility(value) {
     setExitDdVisibility(value)
-  }
-  function SearchBarVisibility(value) {
-    setSearchBarVisibility(value)
   }
 
   return (
@@ -32,23 +24,19 @@ function NavBar({ changeState }) {
 
         leading={props => (
           <IconButton icon={props => <Icon name="menu" {...props} />} {...props}
-            onPress={() => { setDdVisibility(!dropdownVisible), setExitDdVisibility(false), setSearchBarVisibility(false) }} />
+            onPress={() => { setDdVisibility(!dropdownVisible), setExitDdVisibility(false) }} />
         )}
 
         trailing={props => (
           <HStack>
             <IconButton
-              icon={props => <Icon name="magnify" {...props} />} {...props}
-              onPress={() => { setSearchBarVisibility(!searchBarVisible), setExitDdVisibility(false), setDdVisibility(false) }} />
-            <IconButton
               icon={props => <Icon name="dots-vertical" {...props} />} {...props}
-              onPress={() => { setExitDdVisibility(!exitDropdownVisible), setDdVisibility(false), setSearchBarVisibility(false) }} />
+              onPress={() => { setExitDdVisibility(!exitDropdownVisible), setDdVisibility(false) }} />
           </HStack>
         )}
       />
       {dropdownVisible ? <Dropdown changeState={changeState} changeVisibilityState={DropdownVisibility} /> : <></>}
       {exitDropdownVisible ? <ExitDropdown changeState={ExitDdVisibility} /> : <></>}
-      {searchBarVisible ? <SearchBarComp changeState={SearchBarVisibility} /> : <></>}
     </>
   )
 }
